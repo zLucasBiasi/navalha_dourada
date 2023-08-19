dd964f05877714d3c2a69066b44808b1febfffed
zLucasBiasi <lucasbiasi2001@gmail.com>
2023-08-19 00:32:16 -0300

    refactor: resolving build error

--- src/app/livros/page.tsx
+++ src/app/livros/page.tsx
@@ -6,13 +6,14 @@ import { GET_BOOKS } from "@/services/queries";
 
 const Books = () => {
   const { error, data } = useQuery(GET_BOOKS);
+
   if (error) return <p>Erro ao carregar os dados</p>;
 
   return (
     <div className="container mx-auto p-8">
       <h1 className="text-3xl font-semibold mt-32 p-4">Lista de Livros</h1>
       <div>
-        {data.booksApi?.map((book: any, index: any) => (
+        {data?.booksApi?.map((book: any, index: any) => (
           <BookCard key={index} book={book} />
         ))}
       </div>
