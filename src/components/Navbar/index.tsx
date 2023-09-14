599fb9f9c12327fbe04707dcb9defa1ec696b012
zLucasBiasi <lucasbiasi2001@gmail.com>
2023-09-14 12:14:46 -0300

    refactor: change link default to next link

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -44,7 +44,7 @@ const Navbar = () => {
         aria-label="Global"
       >
         <div className="flex lg:flex-1">
-          <a href="/" className="-m-1.5 p-1.5">
+          <Link href="/" className="-m-1.5 p-1.5">
             <div className="w-48 h-14 relative">
               <Image
                 fill
@@ -54,7 +54,7 @@ const Navbar = () => {
                 priority
               />
             </div>
-          </a>
+          </Link>
         </div>
 
         <div className="flex lg:hidden">
@@ -70,13 +70,13 @@ const Navbar = () => {
 
         <div className="hidden lg:flex lg:gap-x-12">
           {navigation.map((item) => (
-            <a
+            <Link
               key={item.name}
               href={item.href}
               className="text-sm font-semibold leading-6 text-white"
             >
               {item.name}
-            </a>
+            </Link>
           ))}
         </div>
 
@@ -101,7 +101,7 @@ const Navbar = () => {
 
         <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-[#0f0f0f] px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="/" className="-m-1.5 p-1.5">
+            <Link href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
               <div className="w-48 h-14 relative">
                 <Image
@@ -112,7 +112,7 @@ const Navbar = () => {
                   priority
                 />
               </div>
-            </a>
+            </Link>
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -126,13 +126,13 @@ const Navbar = () => {
             <div className="-my-6 divide-y divide-gray-500/20">
               <div className="space-y-2 py-6">
                 {navigation.map((item) => (
-                  <a
+                  <Link
                     key={item.name}
                     href={item.href}
                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-[#7D7D7D] hover:bg-gray-50"
                   >
                     {item.name}
-                  </a>
+                  </Link>
                 ))}
               </div>
               <div className="py-6">
