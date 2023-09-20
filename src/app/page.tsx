2f7f49ef53255ffd2ec78ebd517eb3723f9ddfbe
Lucas Biasi <87048120+zLucasBiasi@users.noreply.github.com>
2023-09-20 14:19:13 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ const Home = () => {
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-              Seja bem vindo a nossa biblioteca N.D Marinho!
+              Seja bem vindo a biblioteca N.D Marinho!
             </h1>
             <p className="mt-6 text-lg leading-8 text-[#7D7D7D]">
               Temos uma lista atualizada de livros disponíveis. Confira
